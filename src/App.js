5069b95152346609a974c6e71c1e3c8e074b95c7
Wesley Bernard <wesley.bernard64@gmail.com>
2022-06-14 06:02:39 -0400

    Learning proptypes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <div className="container">
-      <Header title='Omegalol' />
+      <Header title={17} />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
+import PropTypes from 'prop-types';
+
 const Header = (props) => {
     return (
         <header>
             <h1>{props.title}</h1>
         </header>
     )
+}
+
+Header.defaultProps = {
+    title: 'superlol'
+}
+
+Header.propTypes = {
+    title: PropTypes.string
 }
 
 export default Header
