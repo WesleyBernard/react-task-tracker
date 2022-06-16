ed89df9b4fa49abad156f367380c2899ede292ad
Wesley Bernard <wesley.bernard64@gmail.com>
2022-06-15 21:19:49 -0400

    Creating Tasks.js

---
+++ src/components/Tasks.js
@@ -0,0 +1,28 @@
+const tasks = [
+    {
+        id: 1,
+        text: "renew regestration",
+        day: "June 6th at 2:00PM",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Start work",
+        day: "July 25th at 9:00AM",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "MTG with the homies",
+        day: "June 18th at 5:00PM",
+        reminder: false
+    }
+]
+
+const Tasks = () => {
+    return <>
+        {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
+    </>
+}
+
+export default Tasks
