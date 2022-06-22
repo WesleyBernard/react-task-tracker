9644a4158790928fae756fe71a3d73ac46999607
Wesley Bernard <wesley.bernard64@gmail.com>
2022-06-22 13:46:29 -0400

    adding tasks state to app.js and bringing the into tasks.js as props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import { useState } from "react";
 function App() {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: "renew regestration",
+            day: "June 6th at 2:00PM",
+            reminder: true
+        },
+        {
+            id: 2,
+            text: "Start work",
+            day: "July 25th at 9:00AM",
+            reminder: true
+        },
+        {
+            id: 3,
+            text: "MTG with the homies",
+            day: "June 18th at 5:00PM",
+            reminder: false
+        }
+    ])
   return (
     <div className="container">
       <Header />
-        <Tasks />
+        <Tasks tasks={tasks} />
     </div>
   );
 }
